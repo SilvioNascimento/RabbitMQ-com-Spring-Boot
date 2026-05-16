1ea55c53314324cf5ba31c8b300620bf9683cad2
Silvio Nascimento Ribeiro <silvio.estudante19@gmail.com>
2026-05-16 16:44:42 -0300

    feat(controller): criar o controller inicial de Publisher

---
+++ ArthurNetoDev/publisher/src/main/java/com/rmq/example/publisher/controller/Publisher.java
@@ -0,0 +1,27 @@
+package com.rmq.example.publisher.controller;
+
+import com.rmq.example.publisher.service.PublisherService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/publish")
+public class Publisher {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queueName;
+
+    @Autowired
+    private PublisherService publisherService;
+
+    @PostMapping(value = "/text")
+    public void publishText(@RequestBody String text) {
+        System.out.println("Sending message: " + text);
+        publisherService.publisherTextMessage(text, queueName);
+    }
+
+}
